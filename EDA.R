8f85e87e9f031d9cd39910d5077ae99b2686df18
ewalsh <ewalsh@brandeis.edu>
2019-08-20 13:41:56 +0100

    EDA update

--- EDA.R
+++ EDA.R
@@ -11,6 +11,8 @@ require(RJSONIO)
 require(httr)
 require(strucchange)
 require(ROCR)
+require(randomForest)
+require(MASS)
 
 # exploratory data analysis -- get data
 orders <- read.csv("~/QuadPay/orders.csv")
@@ -283,8 +285,117 @@ summary(lm(IsUnpaid~emp+qp1+ap+est+customer_credit_score+customer_age,data=Join1
 
 GLMmod <- glm(IsUnpaid~emp+qp1+ap+est+customer_credit_score+customer_age,family=binomial(link='logit'),data=Join1)
 summary(GLMmod)
+
+Join1Expanded <- cbind(Join1,data.frame(AgeSq=Join1$customer_age^2,CSbreak=Join1$customer_credit_score > 613))
+
+GLMmod <- glm(IsUnpaid~emp+est+customer_credit_score+customer_age+AgeSq+CSbreak,family=binomial(link='logit'),data=Join1Expanded)
+summary(GLMmod)
+
+ID <- grep(TRUE,!is.na(apply(Join1Expanded[,c("IsUnpaid","emp","est","customer_credit_score","customer_age","AgeSq","CSbreak")],1,sum)))
+
+pr <- prediction(predict(GLMmod,type="response"), Join1Expanded$IsUnpaid[ID])
+
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+
+AUCdata <- data.frame(`True Positive Rate`=unlist(attributes(prf)$y.values),
+                      `False Positive Rate`=unlist(attributes(prf)$x.values))
+
+
+ppi <- 250
+Sinfo = 40
+png("~/QuadPay/EDA4_InitialLogit.png",width=20*ppi,height=12*ppi,res=ppi)
+
+ggplot(AUCdata,aes(y=True.Positive.Rate,x=False.Positive.Rate)) +
+  geom_smooth(size=2) +
+  theme_bw() +
+  theme(panel.grid.major.y=element_line(color="grey50",linetype="dashed",size=0.75),
+        panel.grid.major.x=element_line(color="grey50",linetype="dotted",size=0.5),
+        panel.grid.minor=element_blank(),
+        panel.background = element_rect(fill=grey(0.95,0.9)),
+        panel.border=element_rect(color="grey50",linetype="solid",size=1),
+        axis.text=element_text(family="axiformaextrabold",size=Sinfo+3),
+        axis.text.x=element_text(angle=0,hjust=1,face="plain",size=Sinfo-10),
+        axis.text.y=element_text(family="axiformaextrabold",size=Sinfo-5),
+        axis.line = element_line(color="black"),
+        axis.title.x=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=-0.4),
+        axis.title.y=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=1.5),
+        legend.position="right",
+        legend.background=element_blank(),
+        legend.key=element_blank(),
+        legend.title=element_text(face="bold",family="axiformaextrabold",size=Sinfo-5),
+        legend.text=element_text(family="axiformaextrabold",size=Sinfo-20,lineheight=0.25),
+        legend.key.size=unit(.05,"npc"),
+        plot.background = element_rect(fill=grey(0.95,0.9)),
+        plot.title=element_text(family="axiformaextrabold",face="bold",hjust=0.5,size=Sinfo+20),
+        strip.background = element_rect(fill=grey(0.95,0.9),color=NA),
+        strip.text=element_text(family="axiformaextrabold",face="bold",size=rel(3.5))) +
+  ylab("True Positive Rate") +
+  xlab("False Positive Rate") +
+  scale_x_continuous(labels=function(X){paste(X*100,"%",sep="")}) + 
+  scale_y_continuous(labels=function(X){paste(X*100,"%",sep="")})
+
+dev.off()
+
+BaseGLMmod <- glm(IsUnpaid~customer_credit_score+customer_age+AgeSq+CSbreak+order_amount,family=binomial(link='logit'),data=Join1Expanded)
+summary(BaseGLMmod)
+
+ID <- grep(TRUE,!is.na(apply(Join1Expanded[,c("IsUnpaid","customer_credit_score","customer_age","AgeSq","CSbreak","order_amount")],1,sum)))
+
+pr <- prediction(predict(BaseGLMmod,type="response"), Join1Expanded$IsUnpaid[ID])
+
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+
+AUCdata <- data.frame(`True Positive Rate`=unlist(attributes(prf)$y.values),
+                      `False Positive Rate`=unlist(attributes(prf)$x.values))
+
+
+ppi <- 250
+Sinfo = 40
+png("~/QuadPay/EDA4_BaseLogit.png",width=20*ppi,height=12*ppi,res=ppi)
+
+ggplot(AUCdata,aes(y=True.Positive.Rate,x=False.Positive.Rate)) +
+  geom_smooth(size=2) +
+  theme_bw() +
+  theme(panel.grid.major.y=element_line(color="grey50",linetype="dashed",size=0.75),
+        panel.grid.major.x=element_line(color="grey50",linetype="dotted",size=0.5),
+        panel.grid.minor=element_blank(),
+        panel.background = element_rect(fill=grey(0.95,0.9)),
+        panel.border=element_rect(color="grey50",linetype="solid",size=1),
+        axis.text=element_text(family="axiformaextrabold",size=Sinfo+3),
+        axis.text.x=element_text(angle=0,hjust=1,face="plain",size=Sinfo-10),
+        axis.text.y=element_text(family="axiformaextrabold",size=Sinfo-5),
+        axis.line = element_line(color="black"),
+        axis.title.x=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=-0.4),
+        axis.title.y=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=1.5),
+        legend.position="right",
+        legend.background=element_blank(),
+        legend.key=element_blank(),
+        legend.title=element_text(face="bold",family="axiformaextrabold",size=Sinfo-5),
+        legend.text=element_text(family="axiformaextrabold",size=Sinfo-20,lineheight=0.25),
+        legend.key.size=unit(.05,"npc"),
+        plot.background = element_rect(fill=grey(0.95,0.9)),
+        plot.title=element_text(family="axiformaextrabold",face="bold",hjust=0.5,size=Sinfo+20),
+        strip.background = element_rect(fill=grey(0.95,0.9),color=NA),
+        strip.text=element_text(family="axiformaextrabold",face="bold",size=rel(3.5))) +
+  ylab("True Positive Rate") +
+  xlab("False Positive Rate") +
+  scale_x_continuous(labels=function(X){paste(X*100,"%",sep="")}) + 
+  scale_y_continuous(labels=function(X){paste(X*100,"%",sep="")})
+
+dev.off()
+
 # Census API calls
-CensusAPIkey <- # Enter Key Here
+CensusAPIkey <- "8dadaedad2b940dd8ffff397507286b479540d00"
 WebRoot <- "https://api.census.gov/data/2012/ewks?get="
 Var <- "RCPTOT"
 Link1 <- ",OPTAX&for="
@@ -352,15 +463,182 @@ Arts <- data.frame(customer_billing_zip=unique(Arts$X5),
 # Arts <- Arts[grep(TRUE,as.character(Arts$X2) == "A "),c("X5","X1")]
 # colnames(Arts) <- c("customer_billing_zip","Arts_Val")
 
-CensusJoin <- left_join(Retail,Science,by="customer_billing_zip")
-CensusJoin <- left_join(CensusJoin,Education,by="customer_billing_zip")
-CensusJoin <- left_join(CensusJoin,HC,by="customer_billing_zip")
-CensusJoin <- left_join(CensusJoin,Arts,by="customer_billing_zip")
+CensusJoin <- merge(Retail,Science,by="customer_billing_zip",all=TRUE)
+CensusJoin <- merge(CensusJoin,Education,by="customer_billing_zip",all=TRUE)
+CensusJoin <- merge(CensusJoin,HC,by="customer_billing_zip",all=TRUE)
+CensusJoin <- merge(CensusJoin,Arts,by="customer_billing_zip",all=TRUE)
 CensusJoin$customer_billing_zip <- as.character(CensusJoin$customer_billing_zip)
 
 CensusJoin[,-1] <- apply(CensusJoin[,-1],2,function(X){(X-mean(X,na.rm=TRUE))/sd(X,na.rm=TRUE)})
+CensusJoin[,-1] <- apply(CensusJoin[,-1],2,function(X){
+  X[grep(TRUE,is.na(X))] <- 0
+  return(X)
+})
 
-Join1_1 <- Join1
+Join1_1 <- Join1Expanded
 Join1_1$customer_billing_zip <- str_sub(Join1_1$customer_billing_zip,1,5)
 
 Join2 <- left_join(Join1_1,CensusJoin,by="customer_billing_zip")
+
+GLMmod2 <- glm(IsUnpaid~emp+est+customer_credit_score+customer_age+AgeSq+CSbreak+order_amount+Science_Val+Education_Val,
+               family=binomial(link='logit'),data=Join2)
+summary(GLMmod2)
+
+ID2 <- grep(TRUE,!is.na(apply(Join2[,c("IsUnpaid","emp","est","customer_credit_score","customer_age","AgeSq",
+                                      "CSbreak","order_amount","Science_Val","Education_Val")],1,sum)))
+
+pr2 <- prediction(predict(GLMmod2,type="response"), Join2$IsUnpaid[ID2])
+
+prf2 <- performance(pr2, measure = "tpr", x.measure = "fpr")
+plot(prf2)
+
+auc2 <- performance(pr2, measure = "auc")
+auc2 <- auc2@y.values[[1]]
+auc2
+
+AUCdata2 <- data.frame(`True Positive Rate`=c(unlist(attributes(prf)$y.values),unlist(attributes(prf2)$y.values)),
+                      `False Positive Rate`=c(unlist(attributes(prf)$x.values),unlist(attributes(prf2)$x.values)),
+                      type=c(rep("Initial",length(unlist(attributes(prf)$y.values))),rep("Expanded",length(unlist(attributes(prf2)$y.values)))))
+
+ppi <- 250
+Sinfo = 40
+png("~/QuadPay/EDA4_ExpandedLogit.png",width=20*ppi,height=12*ppi,res=ppi)
+
+ggplot(AUCdata2,aes(y=True.Positive.Rate,x=False.Positive.Rate,color=type)) +
+  geom_smooth(size=2) +
+  theme_bw() +
+  theme(panel.grid.major.y=element_line(color="grey50",linetype="dashed",size=0.75),
+        panel.grid.major.x=element_line(color="grey50",linetype="dotted",size=0.5),
+        panel.grid.minor=element_blank(),
+        panel.background = element_rect(fill=grey(0.95,0.9)),
+        panel.border=element_rect(color="grey50",linetype="solid",size=1),
+        axis.text=element_text(family="axiformaextrabold",size=Sinfo+3),
+        axis.text.x=element_text(angle=0,hjust=1,face="plain",size=Sinfo-10),
+        axis.text.y=element_text(family="axiformaextrabold",size=Sinfo-5),
+        axis.line = element_line(color="black"),
+        axis.title.x=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=-0.4),
+        axis.title.y=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=1.5),
+        legend.position="right",
+        legend.background=element_blank(),
+        legend.key=element_blank(),
+        legend.title=element_text(face="bold",family="axiformaextrabold",size=Sinfo-5),
+        legend.text=element_text(family="axiformaextrabold",size=Sinfo-20,lineheight=0.25),
+        legend.key.size=unit(.05,"npc"),
+        plot.background = element_rect(fill=grey(0.95,0.9)),
+        plot.title=element_text(family="axiformaextrabold",face="bold",hjust=0.5,size=Sinfo+20),
+        strip.background = element_rect(fill=grey(0.95,0.9),color=NA),
+        strip.text=element_text(family="axiformaextrabold",face="bold",size=rel(3.5))) +
+  ylab("True Positive Rate") +
+  xlab("False Positive Rate") +
+  scale_x_continuous(labels=function(X){paste(X*100,"%",sep="")}) + 
+  scale_y_continuous(labels=function(X){paste(X*100,"%",sep="")}) +
+  scale_color_manual(values=c(rgb(0, 123/360, 255/360, 0.5),rgb(85/360, 93/360, 102/360, 0.5)))
+
+dev.off()
+
+Join3 <- Join2
+Join3$IsUnpaid <- as.numeric(Join3$IsUnpaid)
+svmfit <- svm(IsUnpaid~emp+est+customer_credit_score+customer_age+AgeSq+CSbreak+order_amount+Retail_Val+Science_Val+Education_Val+HC_Val+Arts_Val,data=Join3,kernel="radial")
+# plot(svmfit,data=Join3[,c("IsUnpaid","emp","est","customer_credit_score","customer_age","AgeSq",
+#                              "CSbreak","order_amount","Retail_Val","Science_Val","Education_Val","HC_Val","Arts_Val")])
+# PCAfit <- princomp(cov(Join1Expanded[,c("IsUnpaid","AmountWtd","emp","est","customer_credit_score","customer_age","AgeSq","CSbreak")],use="pairwise.complete.obs"))
+
+ID3 <- grep(TRUE,!is.na(apply(Join3[,c("IsUnpaid","emp","est","customer_credit_score","customer_age","AgeSq",
+                                       "CSbreak","order_amount","Retail_Val","Science_Val","Education_Val","HC_Val","Arts_Val")],1,sum)))
+# RFfit <- randomForest(AmountWtd~emp+est+customer_credit_score+customer_age+AgeSq+CSbreak+Science_Val+Education_Val,data=Join2[ID2,],
+#                       importance=TRUE,proximity=TRUE)
+  
+pr3 <- prediction(predict(svmfit,type="response"), Join3$IsUnpaid[ID3])
+
+prf3 <- performance(pr3, measure = "tpr", x.measure = "fpr")
+plot(prf3)
+
+auc3 <- performance(pr3, measure = "auc")
+auc3 <- auc3@y.values[[1]]
+auc3
+
+AUCdata3 <- data.frame(`True Positive Rate`=c(unlist(attributes(prf)$y.values),unlist(attributes(prf2)$y.values),unlist(attributes(prf3)$y.values)),
+                       `False Positive Rate`=c(unlist(attributes(prf)$x.values),unlist(attributes(prf2)$x.values),unlist(attributes(prf3)$x.values)),
+                       type=c(rep("Initial Logit",length(unlist(attributes(prf)$y.values))),
+                              rep("Expanded Logit",length(unlist(attributes(prf2)$y.values))),
+                              rep("Support Vector Machine",length(unlist(attributes(prf3)$y.values)))))
+
+ppi <- 250
+Sinfo = 40
+png("~/QuadPay/EDA5_SVM.png",width=20*ppi,height=12*ppi,res=ppi)
+
+ggplot(AUCdata3,aes(y=True.Positive.Rate,x=False.Positive.Rate,color=type)) +
+  geom_line(size=2) +
+  theme_bw() +
+  theme(panel.grid.major.y=element_line(color="grey50",linetype="dashed",size=0.75),
+        panel.grid.major.x=element_line(color="grey50",linetype="dotted",size=0.5),
+        panel.grid.minor=element_blank(),
+        panel.background = element_rect(fill=grey(0.95,0.9)),
+        panel.border=element_rect(color="grey50",linetype="solid",size=1),
+        axis.text=element_text(family="axiformaextrabold",size=Sinfo+3),
+        axis.text.x=element_text(angle=0,hjust=1,face="plain",size=Sinfo-10),
+        axis.text.y=element_text(family="axiformaextrabold",size=Sinfo-5),
+        axis.line = element_line(color="black"),
+        axis.title.x=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=-0.4),
+        axis.title.y=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=1.5),
+        legend.position="right",
+        legend.background=element_blank(),
+        legend.key=element_blank(),
+        legend.title=element_text(face="bold",family="axiformaextrabold",size=Sinfo-5),
+        legend.text=element_text(family="axiformaextrabold",size=Sinfo-20,lineheight=0.25),
+        legend.key.size=unit(.05,"npc"),
+        plot.background = element_rect(fill=grey(0.95,0.9)),
+        plot.title=element_text(family="axiformaextrabold",face="bold",hjust=0.5,size=Sinfo+20),
+        strip.background = element_rect(fill=grey(0.95,0.9),color=NA),
+        strip.text=element_text(family="axiformaextrabold",face="bold",size=rel(3.5))) +
+  ylab("True Positive Rate") +
+  xlab("False Positive Rate") +
+  scale_x_continuous(labels=function(X){paste(X*100,"%",sep="")}) + 
+  scale_y_continuous(labels=function(X){paste(X*100,"%",sep="")}) +
+  scale_color_manual(values=c(rgb(0, 123/360, 255/360, 0.5),rgb(85/360, 93/360, 102/360, 0.5),"black"))
+
+dev.off()
+
+write.csv(Join3,"Join3.csv")
+
+AUCsparkData <- read.csv("AUCdataSpark.csv")[,-1]
+
+AUCsparkData <- rbind(AUCsparkData,AUCdata3)
+AUCsparkData$type <- factor(as.character(AUCsparkData$type),levels=unique(AUCsparkData$type)[c(3:5,1:2)])
+
+ppi <- 250
+Sinfo = 40
+png("~/QuadPay/EDA5_Spark.png",width=20*ppi,height=12*ppi,res=ppi)
+
+ggplot(AUCsparkData,aes(y=True.Positive.Rate,x=False.Positive.Rate,color=type,linetype=type)) +
+  geom_line(size=2) +
+  theme_bw() +
+  theme(panel.grid.major.y=element_line(color="grey50",linetype="dashed",size=0.75),
+        panel.grid.major.x=element_line(color="grey50",linetype="dotted",size=0.5),
+        panel.grid.minor=element_blank(),
+        panel.background = element_rect(fill=grey(0.95,0.9)),
+        panel.border=element_rect(color="grey50",linetype="solid",size=1),
+        axis.text=element_text(family="axiformaextrabold",size=Sinfo+3),
+        axis.text.x=element_text(angle=0,hjust=1,face="plain",size=Sinfo-10),
+        axis.text.y=element_text(family="axiformaextrabold",size=Sinfo-5),
+        axis.line = element_line(color="black"),
+        axis.title.x=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=-0.4),
+        axis.title.y=element_text(face="bold",family="axiformaextrabold",size=Sinfo,vjust=1.5),
+        legend.position="right",
+        legend.background=element_blank(),
+        legend.key=element_blank(),
+        legend.title=element_text(face="bold",family="axiformaextrabold",size=Sinfo-5),
+        legend.text=element_text(family="axiformaextrabold",size=Sinfo-20,lineheight=0.25),
+        legend.key.size=unit(.05,"npc"),
+        plot.background = element_rect(fill=grey(0.95,0.9)),
+        plot.title=element_text(family="axiformaextrabold",face="bold",hjust=0.5,size=Sinfo+20),
+        strip.background = element_rect(fill=grey(0.95,0.9),color=NA),
+        strip.text=element_text(family="axiformaextrabold",face="bold",size=rel(3.5))) +
+  ylab("True Positive Rate") +
+  xlab("False Positive Rate") +
+  scale_x_continuous(labels=function(X){paste(X*100,"%",sep="")}) + 
+  scale_y_continuous(labels=function(X){paste(X*100,"%",sep="")}) +
+  scale_color_manual(values=c(rgb(0, 123/360, 255/360, 0.5),rgb(85/360, 93/360, 102/360, 0.5),"black","darkorange","darkgreen")) +
+  scale_linetype_manual(values=c(rep("solid",3),"dotted","longdash"))
+
+dev.off()
